a8d6a5e31463a608d28d4fbe6662f220d1358034
Hansa Anuradha <hansaanuradha@Hansas-MacBook-Pro.local>
2018-12-04 08:44:41 +0530

    Cheking answer and updating the question has been implemented

--- Qizller/Controller/ViewController.swift
+++ Qizller/Controller/ViewController.swift
@@ -12,7 +12,11 @@ class ViewController: UIViewController {
     
     // Create a QuestionBank object
     let allQuestions = QuestionBank()
-
+    var pickedAnswer : Bool = false
+    
+    // Create a random index to select a random question
+    var randomIndex : Int = 0
+    
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressLabbel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -20,23 +24,52 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Display question
-        displayRandomQuestion()
-    
+        
+        // Start over the app by displaying a new question
+        startOver()
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        print(sender.tag)
-        // Display question
-        displayRandomQuestion()
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        // Check answer
+        checkAnswer()
+        
+        // Start over the app by displaying a new question
+        startOver()
+    }
+    
+    func startOver() {
+        // Create a random index
+        randomIndex = Int.random(in: 0...12)
+        
+        // Select a random question
+        let selectedQuestion : Question = allQuestions.list[randomIndex]
+        
+        // Set the new question text to a label
+        questionLabel.text = selectedQuestion.questionText
+    }
+    
+    func checkAnswer() {
+        
+        // Get the correct answer
+        let correctAnswer = allQuestions.list[randomIndex].answer
+        
+        if pickedAnswer == correctAnswer{
+            print("You got it!!")
+        }
+        else if pickedAnswer != correctAnswer{
+            print("Wrong")
+        }
     }
     
     
-    func displayRandomQuestion() {
-        // Create random index
-        var randomIndex = Int.random(in: 0...12)
-        // Set the randomly selected question text to the label
-        questionLabel.text = allQuestions.list[randomIndex].questionText
-    }
+    
+    
+    
 }
 
