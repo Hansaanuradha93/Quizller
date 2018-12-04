b19d8e19a5140ed6149701e040cee6b872e4da3e
Hansa Anuradha <hansaanuradha@Hansas-MacBook-Pro.local>
2018-12-04 20:31:27 +0530

    added UIAlertController, and other logic

--- Qizller/Controller/ViewController.swift
+++ Qizller/Controller/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Properties to store scores
+    var numberOfQuestionsCorrect: Int = 0
+    var numberOfAllQuestionAnswered: Int = 0
+    var scoreLavel: Int = 0
+
+    
     // Create a QuestionBank object
     let allQuestions = QuestionBank()
     var pickedAnswer : Bool = false
@@ -18,15 +24,18 @@ class ViewController: UIViewController {
     var randomIndex : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var progressLabbel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var progressLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Start over the app by displaying a new question
-        startOver()
+        nextQuestion()
+        
+        // Update Scores to 0
+        updateUI()
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
@@ -37,13 +46,31 @@ class ViewController: UIViewController {
         }
         
         // Check answer
-        checkAnswer()
+        let selectedAnswer = checkAnswer()
         
         // Start over the app by displaying a new question
-        startOver()
+        nextQuestion()
+        
+        // Update score when answered
+        updateUI(answer: selectedAnswer)
     }
     
-    func startOver() {
+    func restart()  {
+        // Restart the questions
+        numberOfAllQuestionAnswered = 0
+        scoreLavel = 0
+        numberOfQuestionsCorrect = 0
+        
+        // Set progress label
+        progressLabel.text = "\(numberOfQuestionsCorrect) / \(numberOfAllQuestionAnswered)"
+        // Set score label
+        scoreLabel.text = "Score: \(scoreLavel)"
+        
+        print("Game Over!")
+    }
+
+    
+    func nextQuestion() {
         // Create a random index
         randomIndex = Int.random(in: 0...12)
         
@@ -54,22 +81,73 @@ class ViewController: UIViewController {
         questionLabel.text = selectedQuestion.questionText
     }
     
-    func checkAnswer() {
+    func checkAnswer() -> Bool{
         
         // Get the correct answer
         let correctAnswer = allQuestions.list[randomIndex].answer
         
         if pickedAnswer == correctAnswer{
             print("You got it!!")
+            return true
         }
         else if pickedAnswer != correctAnswer{
             print("Wrong")
+            return false
         }
+        
+        return false
+    }
+    
+    func updateUI(answer : Bool? = nil) {
+        
+        if answer != nil {
+            if answer == true{
+                // Increase score and progress when answer is correct
+                numberOfQuestionsCorrect += 1
+                scoreLavel += 20
+            }
+            
+            // Increase the value of numberOfAllQuestionAnswered by 1
+            numberOfAllQuestionAnswered += 1
+            
+            // If user has faced 15 questions, give them the chance to end the game or restart it
+            if numberOfAllQuestionAnswered > 14{
+                
+                // Display alert message to the user
+                let alert = UIAlertController(title: "Awesome", message: "You have faced 15 questions already, do you want to start over?", preferredStyle: .alert)
+                
+                let restart = UIAlertAction(title: "Restart", style: .default) { (UIAlertAction) in
+                    // Restart the questions
+                    self.restart()
+                }
+                
+                // Add alert action to the alert
+                alert.addAction(restart)
+                
+                // Present the alert
+                present(alert, animated: true, completion: nil)
+                
+            }
+            
+            
+            // Set progress label
+            progressLabel.text = "\(numberOfQuestionsCorrect) / \(numberOfAllQuestionAnswered)"
+            // Set score label
+            scoreLabel.text = "Score: \(scoreLavel)"
+            
+            
+        } else {
+            // Set progress label
+            progressLabel.text = "\(numberOfQuestionsCorrect) / \(numberOfAllQuestionAnswered)"
+            // Set score label
+            scoreLabel.text = "Score: \(scoreLavel)"
+        }
+        
+        
     }
     
     
-    
-    
+
     
 }
 
